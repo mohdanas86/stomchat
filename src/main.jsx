14261d1ad033edc54c897492cabc23060a716343
mohdanas_86 <anasalam7739@gmail.com>
2024-08-18 21:33:05 +0530

    remove unuse files

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
-import Users from "./components/Users.jsx";
 import Chat from "./components/chat.jsx"
 import Header from "./components/Header.jsx";
-import Footer from "./components/Footer.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -15,7 +13,6 @@ createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/users" element={<Users />} />
         <Route path="/chat" element={<Chat />} />
       </Routes>
     </BrowserRouter>
