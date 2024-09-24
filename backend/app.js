b2ffb9c42aeeea4a394ebbf09b1463c78a0504e3
Anas <anasalam7739@gmail.com>
2024-09-24 17:25:21 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 require('dotenv').config();
 
 const corsOptions = {
-    origin: "http://localhost:3000"
+    origin: "https://stomchat.netlify.app"
 };
 app.use(cors(corsOptions));
 
